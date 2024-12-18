b572f53b05a3d3351895267e463cdd6a18e6776b
Achalogy <achalogy@gmail.com>
2024-12-18 17:40:27 -0500

    Borrar pruebas reto 18

--- adventjs/18/main.js
+++ adventjs/18/main.js
@@ -14,21 +14,3 @@ function findInAgenda(agenda, phone) {
 
   return (results.length == 1) ? results[0][2] : null
 }
-
-const agenda = `+34-600-123-456 Calle Gran Via 12 <Juan Perez>
-Plaza Mayor 45 Madrid 28013 <Maria Gomez> +34-600-987-654
-<Carlos Ruiz> +1-800-555-0199 Fifth Ave New York`
-
-findInAgenda(agenda, '34-600-123-456')
-// // { name: "Juan Perez", address: "Calle Gran Via 12" }
-
-// findInAgenda(agenda, '600-987')
-// // { name: "Maria Gomez", address: "Plaza Mayor 45 Madrid 28013" }
-
-// findInAgenda(agenda, '111')
-// // null
-// // Explicación: No hay resultados
-
-// findInAgenda(agenda, '1')
-// // null
-// // Explicación: Demasiados resultados
